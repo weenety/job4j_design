c42fb946cfc960608aa07d92415ffabc051f9966
isess <isessed@gmaIL.com>
2024-04-21 18:05:55 +0300

    4. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
 
@@ -12,5 +14,20 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthday = Calendar.getInstance();
+        User user1 = new User("Pavel", 23, birthday);
+        User user2 = new User("Pavel", 23, birthday);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        /*
+        Поскольку методы hashCode() и equals() не переопределены в классе User
+        каждый объект будет иметь уникальный хеш-код,
+        и, следовательно, они будут считаться разными ключами, даже если их поля идентичны.
+         */
+        System.out.println(map);
     }
 }
