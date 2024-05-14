e3d2c140936d997891ab9977698ff60dfa66dfd7
isess <isessed@gmaIL.com>
2024-05-14 17:26:37 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 2; i <= 9; i++) {
+                String result = String.format("1 + %d = %d", i, i);
+                output.write(result.getBytes());
+                output.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
