294745c18febcb8ec4a8822a3a6e9e39b0527ae0
isess <isessed@gmaIL.com>
2024-05-19 18:51:44 +0300

    7. Анализ доступности сервера [#859 #496909]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,36 +1,29 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
 
     public void unavailable(String source, String target) {
-        try {
-            List<String> sourceLines = Files.readAllLines(Paths.get(source));
-            String startTime = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(new FileWriter(target))) {
+            String line;
+            StringBuilder result = new StringBuilder();
             boolean serverUnavailable = false;
-            List<String> unavailabilityPeriods = new ArrayList<>();
-            for (String line : sourceLines) {
+            while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(" ");
-                String status = parts[0];
-                String currentTime = parts[1];
-                if (("400".equals(status) || "500".equals(status)) && !serverUnavailable) {
-                    startTime = currentTime;
+                if ((parts[0].equals("400") || parts[0].equals("500")) && !serverUnavailable) {
+                    result.append(parts[1]).append(";");
                     serverUnavailable = true;
-                } else if (("200".equals(status) || "300".equals(status)) && serverUnavailable) {
-                    unavailabilityPeriods.add(startTime + ";" + currentTime + ";");
+                } else if ((parts[0].equals("200") || parts[0].equals("300")) && serverUnavailable) {
+                    result.append(parts[1]).append(";");
+                    writer.println(result);
+                    result.setLength(0);
                     serverUnavailable = false;
-                }
-            }
-            try (PrintWriter printWriter = new PrintWriter(new FileWriter(target))) {
-                for (String interval : unavailabilityPeriods) {
-                    printWriter.println(interval);
                 }
             }
         } catch (IOException e) {
