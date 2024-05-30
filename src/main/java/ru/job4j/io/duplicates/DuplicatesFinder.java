887e1aaa344e18981cfb39fa37b57265cf51dc51
isess <isessed@gmaIL.com>
2024-05-30 18:01:17 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -0,0 +1,38 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DuplicatesFinder extends SimpleFileVisitor<Path> {
+
+    private HashMap<FileProperty, List<Path>> fileMap = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (Files.isRegularFile(file)) {
+            FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
+            fileMap.computeIfAbsent(fileProperty, k -> new ArrayList<>()).add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    private void printDuplicates() {
+        for (List<Path> paths : fileMap.values()) {
+            if (paths.size() > 1) {
+                for (Path path : paths) {
+                    System.out.println(path.toAbsolutePath());
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        DuplicatesFinder finder = new DuplicatesFinder();
+        Files.walkFileTree(Path.of("./"), finder);
+        finder.printDuplicates();
+    }
+}
