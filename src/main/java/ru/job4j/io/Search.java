528bbaea503686d9ab8b7bda86a8f56e35833def
isess <isessed@gmaIL.com>
2024-05-31 17:39:27 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -11,13 +11,26 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (!validateArgs(args)) {
+            throw new IllegalArgumentException("Invalid arguments. Usage: start path, file extension");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static boolean validateArgs(String[] args) {
+        boolean isArgsCorrect = false;
+        if (args.length > 1) {
+            if ((!args[0].isEmpty() && Files.isDirectory(Paths.get(args[0]))) && !args[1].isEmpty()) {
+                isArgsCorrect = true;
+            }
+        }
+        return isArgsCorrect;
     }
 }
